15bd16ec1badb3f2ca1536eca1f846aaf1d92e6f
Frank Hui <huifrank@live.com>
2021-03-19 11:56:05 -0700

    Disable sen analysis

--- sentiment.py
+++ sentiment.py
@@ -5,12 +5,19 @@ import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "victorbot-49929982e4a1.json"
 client = language_v1.LanguageServiceClient()
 
+class sen:
+    score = 0
+    magnitude = 0
+
 def get_sentiment(message):
-    document = language_v1.Document(content=message.content, type_=language_v1.Document.Type.PLAIN_TEXT)
-    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
-    return sentiment
+    # document = language_v1.Document(content=message.content, type_=language_v1.Document.Type.PLAIN_TEXT)
+    # sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+    # return sentiment
+    return sen
+
 
 def get_sentiment_raw(text):
-    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
-    return sentiment
+    # document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+    # sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+    # return sentiment
+    return sen

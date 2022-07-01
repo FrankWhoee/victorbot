07a3555141e6f6e8af909cc04cf850f3263b59b3
Frank Hui <huifrank@live.com>
2022-07-01 11:15:14 -0700

    #19 Add subscribe command

---
+++ commands/subscribe.py
@@ -0,0 +1,15 @@
+import discord
+
+async def main(message: discord.Message, client: discord.Client, data: dict, command: dict):
+    if str(message.guild.id) not in data["guilds"]:
+        data["guilds"][str(message.guild.id)] = {}
+    data["guilds"][str(message.guild.id)]["subscribe_channel"] = message.channel.id
+    await message.channel.send("Set channel to send status updates to.")
+    return True
+
+
+help = {
+    "name": "subscribe",
+    "description": "Sends a status update to this channel every time the bot boots.",
+    "usage": "subscribe"
+}

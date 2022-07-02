ba5f70d70a1824c7ed93c30a1f1cba887a63894a
Frank Hui <huifrank@live.com>
2022-07-02 14:10:44 -0700

    Add error message if guild is not subscribed

--- commands/unsubscribe.py
+++ commands/unsubscribe.py
@@ -1,6 +1,10 @@
 import discord
 
 async def main(message: discord.Message, client: discord.Client, data: dict, command: dict) -> bool:
+    if "subscribe_channel" not in data["guilds"][str(message.guild.id)]:
+        embed = discord.Embed(title="Unsubscribe", description="No channel is subscribed in this guild.", color=0xff0000)
+        await message.channel.send(embed=embed)
+        return False
     del data["guilds"][str(message.guild.id)]["subscribe_channel"]
     embed = discord.Embed(title="Unsubscribe", description="Unsubscribed from this channel.")
     await message.channel.send(embed=embed)

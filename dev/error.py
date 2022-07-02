06c294c6af12117ae5c1f9146e54b29275becccf
Frank Hui <huifrank@live.com>
2022-07-02 01:15:09 -0700

    #1 Add dev commands, improve help, and add error handling/reporting

---
+++ dev/error.py
@@ -0,0 +1,13 @@
+import discord
+
+async def main(message: discord.Message, client: discord.Client, data: dict, command: dict) -> bool:
+    # commands must return a boolean that indicates whether they modified data
+    raise Exception("Error: " + command["command"] + " is not a valid command.")
+    # return False
+
+# commands must include a help dictionary with the following keys: name, description, usage
+help = {
+    "name": "error",
+    "description": "Trigger an error.",
+    "usage": ["error"]
+}
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import importlib
 import json
 import os
+import traceback
 
 from dotenv import load_dotenv
 import discord
@@ -12,10 +13,15 @@ intents = discord.Intents.all()
 
 client = discord.Client(intents=intents)
 
-data = {"guilds":{}}
+data = {"guilds": {}, "dev": False}
+
+
 def save_data():
     with open("data.json", "w") as f:
         json.dump(data, f)
+
+
+# !!! Boot up sequence starts
 
 if os.path.isfile("data.json"):
     with open("data.json", "r") as f:
@@ -32,9 +38,15 @@ else:
     data["boot_time"] = datetime.now().timestamp()
     save_data()
 
+prefix = "!"
+if "prefix" not in data:
+    data["prefix"] = "!"
+    save_data()
+else:
+    prefix = data["prefix"]
 
-prefix = data["prefix"] if "prefix" in data else "!"
 
+# !!! Boot up sequence ends
 
 @client.event
 async def on_ready():
@@ -44,6 +56,16 @@ async def on_ready():
         if "subscribe_channel" in data["guilds"][gid]:
             channel = client.get_channel(data["guilds"][gid]["subscribe_channel"])
             await channel.send(embed=status_embed(client, data))
+
+
+@client.event
+async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    if before.channel is None and after.channel is not None:
+        if str(after.channel.guild.id) in data["guilds"]:
+            if str(member.id) in data["guilds"][str(after.channel.guild.id)]["grants"]:
+                channel = client.get_channel(data["guilds"][str(after.channel.guild.id)]["grants"][str(member.id)])
+                await member.move_to(channel)
+                del data["guilds"][str(after.channel.guild.id)]["grants"][str(member.id)]
 
 
 @client.event
@@ -60,22 +82,52 @@ async def on_message(message):
             else:
                 module = importlib.import_module(f'commands.{command["args"][0]}')
                 help = getattr(module, "help")
-                # create an embed with the help information
                 embed = discord.Embed(title=help["name"], description=help["description"], color=0x00ff00)
-                embed.add_field(name="Usage", value=prefix + help["usage"], inline=False)
+                embed.add_field(name="Usage", value="\n".join([prefix + u for u in help["usage"]]), inline=False)
                 await message.channel.send(embed=embed)
         else:
-            # check if there is a file in commands with the command name
             if os.path.isfile(f'commands/{command["command"]}.py'):
-                # import the file and get the function
                 module = importlib.import_module(f"commands.{command['command']}")
                 func = getattr(module, "main")
-                # call the function with the message and client
-                modifiesData = await func(message, client, data, command)
-                if modifiesData:
-                    save_data()
+
+                await handle_command(command, func, message)
+            elif data["dev"] and os.path.isfile(f'dev/{command["command"]}.py'):
+                module = importlib.import_module(f"dev.{command['command']}")
+                func = getattr(module, "main")
+
+                await handle_command(command, func, message)
             else:
                 await message.channel.send(f"Command {command['command']} not found.")
 
 
-client.run(os.environ.get("discord"))
+async def handle_command(command, func, message):
+    if str(message.guild.id) not in data["guilds"]:
+        data["guilds"][str(message.guild.id)] = {}
+    try:
+        modifiesData = await func(message, client, data, command)
+        if modifiesData:
+            save_data()
+    except discord.errors.Forbidden as e:
+        # create an embed with the error message and send it
+        embed = discord.Embed(title="Permissions insufficient", color=0xff0000)
+        await message.channel.send(embed=embed)
+    except Exception as e:
+        if "owner" in os.environ:
+            embed = discord.Embed(title="An unknown error occurred.",
+                                  description="Error has been reported to the owner.", color=0xff0000)
+            await message.channel.send(embed=embed)
+            # create an embed with the error message and author and time stamp
+            embed = discord.Embed(title="Error", description=traceback.format_exc(), color=0xff0000)
+            embed.set_author(name=message.author.name, icon_url=message.author.avatar.url)
+            embed.add_field(name="Message", value=message.content, inline=False)
+            embed.set_footer(text=f"{message.created_at}")
+            await client.get_user(int(os.environ["owner"])).send(embed=embed)
+        else:
+            embed = discord.Embed(title="An unknown error occurred.", color=0xff0000)
+            await message.channel.send(embed=embed)
+
+
+if os.environ.get("dev").lower() == "true":
+    client.run(os.environ.get("devkey"))
+else:
+    client.run(os.environ.get("prodkey"))

1b5e274c446989fc0cafdc6ea434e8d9233b1893
FrankWhoee <huifrank@live.com>
2022-07-29 18:40:07 -0700

    #16 Add message reaction

--- reactions/message.py
+++ reactions/message.py
@@ -1,5 +1,14 @@
 import discord
 
-async def main(reaction: discord.Reaction, user: discord.User, client: discord.Client, data: dict, command: dict) -> bool:
-    # reactions must return a boolean that indicates whether they modified data
-    return False
+# data will always be assumed to be modified, and thus saved.
+async def main(reaction: discord.Reaction, user: discord.User, client: discord.Client, data: dict, obj: dict):
+    # it is the main thread's responsibility to delete the object in data
+    if reaction.emoji == "✅":
+        # get the channel and message from the data
+        channel = client.get_channel(obj["args"][0])
+        message = obj["args"][1]
+        # send the message
+        await channel.send(message)
+    await reaction.message.remove_reaction("❌", client.user)
+    await reaction.message.remove_reaction("✅", client.user)
+    return True

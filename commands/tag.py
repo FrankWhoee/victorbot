a07139a6be3b44b8284606c19b32359203b4496a
FrankWhoee <huifrank@live.com>
2022-08-20 22:37:25 -0700

    #63 Add messageid as valid param and update help

--- commands/tag.py
+++ commands/tag.py
@@ -5,9 +5,14 @@ import discord
 
 async def main(message: discord.Message, client: discord.Client, data: dict, command: dict,
                sqldb: sqlite3.Cursor) -> bool:
-    if message.reference is not None:
+    if len(command["args"]) == 2:
+        target = await message.channel.fetch_message(command["args"][0])
+        tag = command["args"][1]
+    elif message.reference is not None:
         target = await message.channel.fetch_message(message.reference.message_id)
+        tag = command["args"][0]
     else:
+        tag = command["args"][0]
         nextflag = False
         size = 5
         while not nextflag and size < 50:
@@ -21,7 +26,7 @@ async def main(message: discord.Message, client: discord.Client, data: dict, com
         size += 5
     try:
         sqldb.execute("INSERT INTO tags(messageId, guildId, channelId, tag, content, link, authorId) VALUES (?,?,?,?,?,?,?)",
-                  (target.id, target.guild.id, target.channel.id, command["args"][0], target.content, target.jump_url, target.author.id))
+                      (target.id, target.guild.id, target.channel.id, tag, target.content, target.jump_url, target.author.id))
         embed = discord.Embed(title="Success", description="Message tagged.", color=0x00ff00)
         await message.channel.send(embed=embed)
     except:
@@ -32,6 +37,6 @@ async def main(message: discord.Message, client: discord.Client, data: dict, com
 # commands must include a help dictionary with the following keys: name, description, usage
 help = {
     "name": "tag",
-    "description": "Tags message with <tag>.",
-    "usage": ["tag <tag>"]
+    "description": "Tags message with <tag>. If a <messageid> is provided, <messageid> is tagged. If the command replies to a message, the replied message will be tagged, otherwise the next message up is tagged.",
+    "usage": ["tag <tag>", "tag <messageid> <tag>"]
 }
